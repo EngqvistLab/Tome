539b54ca1abac2c6d00ba2854b80fa90afaac101
Gang Li <gangl@chalmers.se>
2018-11-21 22:41:13 +0100

    Update update_seqs.py

--- update_seqs.py
+++ update_seqs.py
@@ -75,7 +75,7 @@ def main():
         for res in results: fhand.write(res+'\n')
 
         k = i*100
-        if k%1500 == 0:
+        if k%10000 == 0:
             fhand.close()
             file_index += 1
             outfile = os.path.join(outdir,'all_enzymes_{0}.fasta'.format(file_index))
