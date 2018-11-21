36fd06b36b892e2e65f245efa39d5027372f3baa
Gang Li <gangl@chalmers.se>
2018-11-21 15:52:29 +0100

    Update update_seqs.py

--- update_seqs.py
+++ update_seqs.py
@@ -8,9 +8,10 @@ Gang Li
 
 import urllib2
 import sys
+from multiprocessing import Pool, cpu_count
 
 def print_out(line):
-    sys.stdout.write(str(line))
+    sys.stdout.write(str(line)+'\n')
 
 def retrive_from_uniprot(uniprot_id):
     # get fasta content for given uniprot_id, return as string format
@@ -24,13 +25,24 @@ def main():
     outfile = './external_data/all_enzymes.fasta'
 
     fhand = open(outfile,'w')
-    k = 0
+    uniprot_ids = list()
+    numids = len(uniprot_ids)
     for line in open(uniprot_id_file):
         if line.startswith('ec'): continue
         uniprot_id = line.split()[1]
-        rec = retrive_from_uniprot(uniprot_id)
-        fhand.write(rec+'\n')
-        k += 1
+        uniprot_ids.append(uniprot_id)
+
+    num_cpus = cpu_count()
+    print_out('Number of cpus: {0}'.format(num_cpus))
+    for i in range(numids/num_cpus+1):
+        if i < numids/num_cpus:
+            results = Pool(num_cpus).map(retrive_from_uniprot, uniprot_ids[20*i:20*(i+1)])
+        else:
+            results = Pool(num_cpus).map(retrive_from_uniprot, uniprot_ids[20*i:])
+
+        for res in results: fhand.write(res+'\n')
+
+        k = i*num_cpus
         if k%1000 == 0: print_out('{0} seqeunces have been downloaded'.format(k))
     print_out('complete!')
     fhand.close()
