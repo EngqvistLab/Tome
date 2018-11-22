80e72e3932e3ef1aa181ca1ef2f12f712a968bb9
Gang Li <gangl@chalmers.se>
2018-11-22 15:55:09 +0100

    tome

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-import urllib,urllib2,sys
-uniprot_id = 'P12345'
-url = 'https://www.uniprot.org/uniprot/{0}.fasta'.format(uniprot_id)
 
-response = urllib2.urlopen(url)
-
-page = response.read(200000)
-
-def print_out(line):
-    sys.stdout.write(str(line))
-
-print_out(page)
+print '{0},{0},{1},{2}'.format(1,2,3)
